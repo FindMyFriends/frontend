7d22c78f66f116866e303164199b19e1548fbcd8
Dominik Klapuch <klapuchdominik@gmail.com>
2017-10-08 21:29:49 +0200

    Actions

--- src/demands/reducer.js
+++ src/demands/reducer.js
@@ -1,10 +1,12 @@
-import { FETCH_ALL_DEMANDS } from './actions';
+import { RECEIVED_ALL_DEMANDS, REQUESTED_ALL_DEMANDS } from './actions';
 
 const demands = (state = {}, action) => {
-  const { demands, type } = action;
+  const { type } = action;
   switch (type) {
-    case FETCH_ALL_DEMANDS:
-      return demands;
+    case RECEIVED_ALL_DEMANDS:
+      return action.demands;
+    case REQUESTED_ALL_DEMANDS:
+      return state;
     default:
       return state;
   }
