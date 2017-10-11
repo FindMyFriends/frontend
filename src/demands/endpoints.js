0ef2795a4e42ad1a71ec71f6d8d9663a7f83d500
Dominik Klapuch <klapuchdominik@gmail.com>
2017-10-11 20:26:11 +0200

    Dispatch to single line:

--- src/demands/endpoints.js
+++ src/demands/endpoints.js
@@ -2,20 +2,16 @@ import { unauthorized } from './../http/requests';
 import baseUrl from './../http/baseUrl';
 import { receivedAll, requestedAll, receivedSingle, requestedSingle } from './actions';
 
-export const all = () => {
-  return dispatch => {
-    dispatch(requestedAll());
-    fetch(baseUrl('/v1/demands'), unauthorized())
-    .then(response => response.json())
-    .then(demands => dispatch(receivedAll(demands)));
-  };
+export const all = () => dispatch => {
+  dispatch(requestedAll());
+  fetch(baseUrl('/v1/demands'), unauthorized())
+  .then(response => response.json())
+  .then(demands => dispatch(receivedAll(demands)));
 };
 
-export const single = id => {
-  return dispatch => {
-    dispatch(requestedSingle(id));
-    fetch(baseUrl(`/v1/demands/${id}`), unauthorized())
-    .then(response => response.json())
-    .then(demand => dispatch(receivedSingle(id, demand)));
-  };
+export const single = id => dispatch => {
+  dispatch(requestedSingle(id));
+  fetch(baseUrl(`/v1/demands/${id}`), unauthorized())
+  .then(response => response.json())
+  .then(demand => dispatch(receivedSingle(id, demand)));
 };
