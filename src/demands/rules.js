c543cd5ab8835e86d9c80d2beb45fb1d8260a54c
Dominik Klapuch <klapuchdominik@gmail.com>
2017-10-23 19:24:01 +0200

    Added rules

---
+++ src/demands/rules.js
@@ -0,0 +1,36 @@
+export const validatedRace = (race, races) => {
+  if (races.includes(race)) {
+    return race;
+  }
+  throw new Error(`Race ${race} is not allowed`);
+};
+
+export const validatedGender = (gender, genders) => {
+  if (genders.includes(gender)) {
+    return gender;
+  }
+  throw new Error(`Gender ${gender} is not allowed`);
+};
+
+export const validatedAge = (age) => {
+  const splitted = age.split(',');
+  const ranges = { from: splitted[0].slice(1), to: splitted[1].slice(0, -1) };
+  if (ranges.from > ranges.to) {
+    throw new Error('Ages are swapped');
+  }
+  return age;
+};
+
+const validatedDemand = (demand, selects) => {
+  const { races, genders } = selects;
+  const { demand: { general } } = demand;
+  return {
+    general: {
+      race: validatedRace(general.race, races),
+      gender: validatedGender(general.gender, genders),
+      age: validatedAge(general.age),
+    },
+  };
+};
+
+export default validatedDemand;
