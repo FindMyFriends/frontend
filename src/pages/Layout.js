82bbd89abbdb6ac6bccf97e7a1c603d7d28cfccd
Dominik Klapuch <klapuchdominik@gmail.com>
2017-11-26 15:33:22 +0100

    Common layout

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Route } from 'react-router-dom';
 import Header from './Header';
 
-const Layout = ({ children }) => (
-  <div>
-    <Header />
-    <div className="container-fluid text-center">
-      <div className="row content">
-        <div className="col-sm-10 col-sm-offset-1 text-left">
-          {children}
+const Layout = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={matchProps => (
+      <div>
+        <Header />
+        <div className="container-fluid text-center">
+          <div className="row content">
+            <div className="col-sm-10 col-sm-offset-1 text-left">
+              <Component {...matchProps} />
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
+  )}
+  />
 );
 
 Layout.propTypes = {
-  children: PropTypes.element,
+  component: PropTypes.element,
 };
 
 export default Layout;
