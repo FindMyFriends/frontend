3bc963093c29ef564d74a6ddb1ed560e2920e2d3
Dominik Klapuch <klapuchdominik@gmail.com>
2018-02-10 11:01:18 +0100

    Added flow

--- tests/schema.ava.js
+++
@@ -1,9 +0,0 @@
-import test from 'ava';
-import { replaceNull } from './../src/schema';
-
-test('replacing null to string', (t) => {
-  t.deepEqual(
-    ['a', 'b', 'c'],
-    replaceNull(['a', 'b', null], 'c'),
-  );
-});
