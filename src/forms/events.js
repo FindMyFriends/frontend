5945ab05266cc7ba0a55e12e8a01e63114dc01ac
Dominik Klapuch <klapuchdominik@gmail.com>
2018-02-10 11:01:18 +0100

    Added flow

--- src/forms/events.js
+++ src/forms/events.js
@@ -1,22 +1,38 @@
 // @flow
 import { id as enumId } from './../enum';
 
-export const onSelectChange = (classic: (x: Object) => any, name: string) => (event: Object, index: any, value: any) => {
+export const onSelectChange = (
+  classic: (x: Object) => any,
+  name: string,
+) => (event: Object, index: any, value: any) => {
   return classic({ target: { name, value } });
 };
 
-export const onSelectEnumChange = (classic: (x: Object) => any, name: string, selection: Object) => (event: Object, index: any, value: any) => {
+export const onSelectEnumChange = (
+  classic: (x: Object) => any,
+  name: string,
+  selection: Object,
+) => (event: Object, index: any, value: any) => {
   return classic({ target: { name, value: enumId(value, selection.id, selection.name) } });
 };
 
-export const onSlideChange = (classic: (x: Object) => any, name: string) => (self: any, value: any) => {
+export const onSlideChange = (
+  classic: (x: Object) => any,
+  name: string,
+) => (self: any, value: any) => {
   return classic({ target: { name, value } });
 };
 
-export const onRangeChange = (classic: (x: Object) => any, name: string, ranges: [number, number]) => (range: [number, number]) => {
+export const onRangeChange = (
+  classic: (x: Object) => any,
+  name: string, ranges: [number, number],
+) => (range: [number, number]) => {
   classic({ target: { name, value: { [ranges[0]]: range[0], [ranges[1]]: range[1] } } });
 };
 
-export const onCheck = (classic: (x: Object) => any, name: string) => (self: any, checked: bool) => {
+export const onCheck = (
+  classic: (x: Object) => any,
+  name: string,
+) => (self: any, checked: bool) => {
   return classic({ target: { name, value: checked } });
 };
