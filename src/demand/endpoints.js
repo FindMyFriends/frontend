dd8599066ce24cd8f2ef015876f955a878f3b1e1
Dominik Klapuch <klapuchdominik@gmail.com>
2018-02-13 22:17:53 +0100

    Type hints

--- src/demand/endpoints.js
+++ src/demand/endpoints.js
@@ -1,3 +1,4 @@
+// @flow
 import axios from 'axios';
 import omit from 'lodash/omit';
 import {
@@ -16,17 +17,20 @@ import {
 import { receivedApiError, receivedSuccess as receivedSuccessMessage } from './../ui/actions';
 import { loadOptions, loadSchema } from './../api/schema';
 
-export const options = () => (dispatch) => {
+export const options = () => (dispatch: (mixed) => Object) => {
   return loadOptions('/v1/demands')
     .then(options => dispatch(receivedOptions(options)));
 };
 
-export const schema = () => (dispatch) => {
+export const schema = () => (dispatch: (mixed) => Object) => {
   return loadSchema('/schema/v1/demand/get.json')
     .then(schema => dispatch(receivedSchema(schema)));
 };
 
-export const all = (pagination = { page: 1, perPage: 20 }) => (dispatch) => {
+export const all = (pagination: Object = {
+  page: 1,
+  perPage: 20,
+}) => (dispatch: (mixed) => Object) => {
   dispatch(requestedAll());
   axios.get(`/v1/demands?page=${pagination.page}&per_page=${pagination.perPage}`)
     .then((response) => {
@@ -35,7 +39,7 @@ export const all = (pagination = { page: 1, perPage: 20 }) => (dispatch) => {
     });
 };
 
-export const single = id => (dispatch) => {
+export const single = (id: string) => (dispatch: (mixed) => Object) => {
   dispatch(requestedSingle(id));
   return axios.get(`/v1/demands/${id}`)
     .then((response) => {
@@ -44,7 +48,7 @@ export const single = id => (dispatch) => {
     });
 };
 
-export const add = (demand, history) => (dispatch) => {
+export const add = (demand: Object, history: Object) => (dispatch: (mixed) => Object) => {
   dispatch(requestedAdding(demand));
   return axios.post('/v1/demands', demand)
     .then((response) => {
@@ -55,7 +59,12 @@ export const add = (demand, history) => (dispatch) => {
     .catch(error => dispatch(receivedApiError(error)));
 };
 
-export const reconsider = (id, demand, etag, history) => (dispatch) => {
+export const reconsider = (
+  id: string,
+  demand: Object,
+  etag: string,
+  history: Object,
+) => (dispatch: (mixed) => Object) => {
   const reconsidered = omit(omit(omit(demand, 'created_at'), 'id'), 'seeker_id');
   dispatch(requestedReconsidering(id, reconsidered));
   axios.put(`/v1/demands/${id}`, reconsidered, { headers: { 'if-match': etag } })
