121d194dc1b70d2ddd80033032e67ce297b82673
Dominik Klapuch <klapuchdominik@gmail.com>
2018-05-06 10:08:19 +0200

    Stars replaced to dots

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -1,9 +1,40 @@
 import React from 'react';
 import Rater from 'react-rater';
+import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import 'react-rater/lib/react-rater.css';
 import { onRating } from './../forms/events';
 import { MaterialLabel } from './Label';
+
+const Dot = styled.span`
+  height: 11px;
+  margin-right: 1px;
+  width: 11px;
+  background-color: #bbb;
+  border-radius: 50%;
+  display: inline-block;
+`;
+
+const ClickableDot = Dot.extend`
+  cursor: pointer;
+`;
+
+const EmptyDot = Dot.extend`
+  height: 5px;
+  width: 5px;
+  background-color: #fff;
+  border-color: #bbb;
+  border-style: solid;
+`;
+
+const CustomRating = ({ willBeActive, isActive }) => {
+  if (isActive) {
+    return <Dot />;
+  } else if (willBeActive) {
+    return <ClickableDot />;
+  }
+  return <EmptyDot />;
+};
 
 export const InputRating = ({
   max, current, name, onChange, children,
@@ -15,7 +46,9 @@ export const InputRating = ({
       total={max}
       rating={current}
       onRate={event => onRating(onChange, name, event)}
-    />
+    >
+      <CustomRating />
+    </Rater>
   </React.Fragment>
 );
 
@@ -24,7 +57,9 @@ export const OutputRating = ({ max, current }) => (
     total={max}
     rating={current}
     interactive={false}
-  />
+  >
+    <CustomRating />
+  </Rater>
 );
 
 InputRating.propTypes = {
@@ -38,4 +73,9 @@ InputRating.propTypes = {
 OutputRating.propTypes = {
   max: PropTypes.number.isRequired,
   current: PropTypes.number.isRequired,
+};
+
+CustomRating.propTypes = {
+  willBeActive: PropTypes.bool,
+  isActive: PropTypes.bool,
 };
