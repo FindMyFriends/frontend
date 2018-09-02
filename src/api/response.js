e50988fff474dd8904e6f8cdcf6fe49e74c34e28
Dominik Klapuch <klapuchdominik@gmail.com>
2018-09-02 22:09:16 +0200

    WIP

--- src/api/response.js
+++ src/api/response.js
@@ -1,3 +1,3 @@
 // @flow
 export const extractedLocationId = (location: string): string | number => location.substring(location.lastIndexOf('/') + 1);
-export const extractedTotalCount = (headers: Array<Object>): number => parseInt(headers['x-total-count'], 10);
+export const extractedTotalCount = (headers: Object): number => parseInt(headers['x-total-count'], 10);
