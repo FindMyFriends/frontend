4a38aa1038a6be46b8f7b2287a8d4ba2d594cdaf
Dominik Klapuch <klapuchdominik@gmail.com>
2018-09-08 13:53:54 +0200

    Reducers to reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,11 @@
 // @flow
 import { combineReducers } from 'redux';
-import demand from '../demand/reducers';
-import message from '../ui/reducers';
-import schema from '../schema/reducers';
-import soulmate from '../soulmate/reducers';
-import evolution from '../evolution/reducers';
-import spot from '../spot/reducers';
+import demand from '../demand/reducer';
+import message from '../ui/reducer';
+import schema from '../schema/reducer';
+import soulmate from '../soulmate/reducer';
+import evolution from '../evolution/reducer';
+import spot from '../spot/reducer';
 
 export default combineReducers({
   demand,
