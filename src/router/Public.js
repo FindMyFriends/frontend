df02f48e5db08824a0c012ff43ff88bd13c268a7
Dominik Klapuch <klapuchdominik@gmail.com>
2018-09-17 00:04:15 +0200

    block some pages after login

--- src/router/Public.js
+++ src/router/Public.js
@@ -2,19 +2,28 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import MainAppBar from '../pages/Layout/menu/MainAppBar';
+import Error404 from '../pages/Error/Error404';
 import Notification from '../ui/Notification';
+import * as session from '../access/session';
 
 type Props = {
   +component: any,
+  +restrictive?: boolean,
 };
-export default ({ component: Component, ...rest }: Props): Route => (
+const Public = ({ component: Component, restrictive = false, ...rest }: Props): Route => (
   <Route
     {...rest}
     render={props => (
-      <React.Fragment>
-        <Notification />
-        <MainAppBar component={<Component {...props} />} />
-      </React.Fragment>
+      restrictive && session.exists()
+        ? <Public component={Error404} {...props} />
+        : (
+          <React.Fragment>
+            <Notification />
+            <MainAppBar component={<Component {...props} />} />
+          </React.Fragment>
+        )
     )}
   />
 );
+
+export default Public;
